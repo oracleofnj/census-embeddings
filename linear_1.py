6f26e0a23620211514ba54e15afd03ce2ceedaa4
oracleofnj <jared.samet@aya.yale.edu>
2019-08-07 16:26:24 -0400

    Flake8

--- linear_1.py
+++ linear_1.py
@@ -1,12 +1,12 @@
 import numpy as np
 import torch
 from torch import nn, optim
-from torch.autograd import Variable
 
 np.random.seed(42)
 x = np.random.rand(50)
 y_train_1 = torch.Tensor(2 * x + 0.5).view(-1, 1)
 y_train_2 = torch.Tensor((-1) * x).view(-1, 1)
+
 
 class LinearRegressionModule(nn.Module):
     def __init__(self, input_dim, output_dim):
@@ -16,6 +16,7 @@ class LinearRegressionModule(nn.Module):
     def forward(self, x):
         out = self.linear(x)
         return out
+
 
 class EmbeddingModule(nn.Module):
     def __init__(self, num_items, embedding_dim):
@@ -27,16 +28,21 @@ class EmbeddingModule(nn.Module):
         out = self.embeddings(x)
         return out
 
+
 class LinearEmbeddingModule(nn.Module):
     def __init__(self, embeddings, output_dim):
         super().__init__()
         self.embeddings = embeddings
-        self.linear = LinearRegressionModule(embeddings.embedding_dim, output_dim)
+        self.linear = LinearRegressionModule(
+            embeddings.embedding_dim,
+            output_dim
+        )
 
     def forward(self, x):
         embeddings = self.embeddings(x)
         out = self.linear(embeddings)
         return out
+
 
 embeddings = EmbeddingModule(50, 1)
 model_1 = LinearEmbeddingModule(embeddings, 1)
@@ -46,7 +52,7 @@ optimizer = optim.SGD([
     {'params': model_1.linear.parameters()},
     {'params': model_2.linear.parameters()},
     {'params': model_1.embeddings.parameters(), 'lr': 0.5},
-], lr = 0.1)
+], lr=0.1)
 
 indices = torch.LongTensor([x for x in range(50)])
 
@@ -64,7 +70,11 @@ for epoch in range(2000):
     optimizer.step()
 
     if ((epoch + 1) % 100) == 0:
-        print('epoch {0}, loss 1 {1}, loss 2 {2}'.format(epoch, loss_1, loss_2))
+        print('epoch {0}, loss 1 {1}, loss 2 {2}'.format(
+            epoch,
+            loss_1,
+            loss_2
+        ))
 
 print(model_1.linear.state_dict())
 print(model_2.linear.state_dict())
