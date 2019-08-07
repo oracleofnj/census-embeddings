c681b20f87f0fc563e5c5573ccc72f1f9b0598e7
oracleofnj <jared.samet@aya.yale.edu>
2019-08-07 17:33:23 -0400

    Bug

--- linear_1.py
+++ linear_1.py
@@ -1,11 +1,7 @@
 import numpy as np
 import torch
 from torch import nn, optim
-
-np.random.seed(42)
-x = np.random.rand(50)
-y_train_1 = torch.Tensor(2 * x + 0.5).view(-1, 1)
-y_train_2 = torch.Tensor((-1) * x).view(-1, 1)
+from torch.utils import data
 
 
 class LinearRegressionModule(nn.Module):
@@ -44,6 +40,28 @@ class LinearEmbeddingModule(nn.Module):
         return out
 
 
+class LinearDataset(data.Dataset):
+    def __init__(self, x, alpha, beta):
+        super().__init__()
+        self.x = x
+        self.alpha = alpha
+        self.beta = beta
+
+    def __len__(self):
+        return len(self.x)
+
+    def __getitem__(self, index):
+        y = self.alpha + self.beta * self.x[index]
+        return torch.LongTensor([index]), y
+
+
+np.random.seed(42)
+x = np.random.rand(50)
+y_1 = LinearDataset(x, 2, 0.5)
+y_2 = LinearDataset(x, -1, 0)
+y_gen_1 = data.DataLoader(y_1, batch_size=10)
+y_gen_2 = data.DataLoader(y_2, batch_size=10)
+
 embeddings = EmbeddingModule(50, 1)
 model_1 = LinearEmbeddingModule(embeddings, 1)
 model_2 = LinearEmbeddingModule(embeddings, 1)
@@ -54,20 +72,26 @@ optimizer = optim.SGD([
     {'params': model_1.embeddings.parameters(), 'lr': 0.5},
 ], lr=0.1)
 
-indices = torch.LongTensor([x for x in range(50)])
+for epoch in range(2):
+    for indices, labels in y_gen_1:
+        print(indices)
+        print(labels)
+        print(indices.shape)
+        print(labels.shape)
+        optimizer.zero_grad()
+        outputs_1 = model_1.forward(indices)
+        print(outputs_1)
+        print(outputs_1.shape)
+        loss_1 = criterion(outputs_1, labels)
+        loss_1.backward()
+        optimizer.step()
 
-for epoch in range(2000):
-    optimizer.zero_grad()
-    outputs_1 = model_1.forward(indices)
-    loss_1 = criterion(outputs_1, y_train_1)
-    loss_1.backward()
-    optimizer.step()
-
-    optimizer.zero_grad()
-    outputs_2 = model_2.forward(indices)
-    loss_2 = criterion(outputs_2, y_train_2)
-    loss_2.backward()
-    optimizer.step()
+    for indices, labels in y_gen_2:
+        optimizer.zero_grad()
+        outputs_2 = model_1.forward(indices)
+        loss_2 = criterion(outputs_2, labels)
+        loss_2.backward()
+        optimizer.step()
 
     if ((epoch + 1) % 100) == 0:
         print('epoch {0}, loss 1 {1}, loss 2 {2}'.format(
